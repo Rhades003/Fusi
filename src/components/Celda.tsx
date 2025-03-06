8be484e45340ad4d837df8c24c84a4966a91d1d1
Rubén <rjulian@elpuig.xeill.net>
2025-03-06 20:46:58 +0100

    component celda

---
+++ src/components/Celda.tsx
@@ -0,0 +1,19 @@
+
+
+function Celda({colorBtn, onClick, text}: {colorBtn:string; onClick: () => void; text:string}) {
+  
+    return (
+      <>
+        <div>
+          <button
+          style={{backgroundColor: colorBtn}}
+          color={colorBtn}
+          onClick={onClick}>
+            {text}
+          </button>
+        </div>
+      </>
+    )
+  }
+  
+  export default Celda;
